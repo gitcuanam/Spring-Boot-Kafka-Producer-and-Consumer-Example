858928edc4dc031bfdd465cb10512f6fd47611c0
gitcuanam <nam.tranphuong.evotek@gmail.com>
2023-04-11 13:34:31 +0700

    split producer and consumer

---
+++ subscribing-server/src/main/java/com/netsurfingzone/dto/Student.java
@@ -0,0 +1,12 @@
+package com.netsurfingzone.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+	private Long id;
+	private String name;
+	private String rollNumber;
+}
